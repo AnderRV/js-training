7ea223fdb42d1ca8a3b365c3c28fdb9d29e03c47
Ander Rodriguez <AnderRodriguezVarela@gmail.com>
2016-11-05 13:12:31 +0100

    Added lib functions to test

--- test.js
+++ test.js
@@ -3,3 +3,21 @@ describe('Basic equalities', function() {
         expect(1).toBe(1);
     });
 });
+
+describe('lib functions', function() {
+    describe('sum', function () {
+        it('should sum two numbers', function () {
+            var result = sum(1, 2);
+
+            expect(result).toBe(3);
+        });
+    });
+
+    describe('subtract', function () {
+        it('should subtract second number from the first one', function () {
+            var result = subtract(3, 2);
+
+            expect(result).toBe(1);
+        });
+    });
+});
